1f0da4d2db10367c50b4d9050d9bb657fe4ec234
SHAAHLOM76 <mahedihossin130@gmail.com>
2025-06-13 21:58:08 +0600

    Update 30.shourov.js

--- scripts/commands/30.shourov.js
+++ scripts/commands/30.shourov.js
@@ -20,7 +20,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["--SHAIRI-VIDEO-𝐊𝐢𝐧𝐠_𝐒𝐡𝐨𝐮𝐫𝐨𝐯-"];
+   var hi = ["--SHAIRI-VIDEO-⃝—͟͟͞͞ 𝐂.𝐄.𝐎⸙𝐒𝐇𝐀𝐀𝐇𝐋𝐎𝐌𓆪___//🩷"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
